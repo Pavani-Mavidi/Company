9354a529135f52c455d60b4bb61fa08c2c17a779
mavip <mavip@L31158WIN.rpega.com>
2018-07-27 15:47:48 +0530

    Added some changes

--- src/EmployeeTest.java
+++ src/EmployeeTest.java
@@ -4,5 +4,6 @@ public class EmployeeTest {
 	public static void main(String args[])
 	{
 		Employee emp=new Employee("Pavani","mavidipavani@gmail.com",635606,"8096683736");
+		System.out.println(emp.getName());
 	}
 }
